d4f01de652ee5229a7b08fd15c46552eb470543c
yakupibrahimbas <ibrahimbasyakup@gmail.com>
2024-01-23 12:54:00 +0300

    o23-3

---
+++ src/test/java/day07_xpath_cssSelector/C03_RadioButton.java
@@ -0,0 +1,44 @@
+package day07_xpath_cssSelector;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C03_RadioButton {
+    // https://www.facebook.com adresine gidin
+    // Cookies'i kabul edin
+    // "Create an Account" button'una basin
+    // "radio buttons" elementlerini locate edin
+    // Secili degilse cinsiyet butonundan size uygun olani secin
+    WebDriver driver;
+    @BeforeEach
+    public void setUp() {
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+}
+    @Test
+    public void test01() throws InterruptedException {
+        driver.get("https://www.facebook.com");
+        driver.findElement(By.xpath("(//a[@role='button'])[2]")).click();
+        Thread.sleep(3000);
+       WebElement female= driver.findElement(By.xpath("//input[@value='1']"));
+       WebElement male= driver.findElement(By.xpath("//input[@value='2']"));
+
+       if (!male.isSelected()){
+           male.click();
+       }
+    }
+    @AfterEach
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
+}
